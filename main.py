a929d6d17e72df6420f833a2e90567a0fd0ec8ce
Alec German <kimarimt@protonmail.com>
2024-10-04 18:00:20 -0400

    add basic game structure

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+from random import randint
+
+
 def main():
-    pass
+    print('''
+================================
+|      ROCK PAPER SCISSORS     |
+================================
+|       '0' for Rock           |
+|       '1' for Paper          |
+|       '2' for Scissors       |
+================================
+''')
+
+    player = int(input('ROCK, PAPER, SCISSORS, SHOOT!: '))
+    print(f'Player chose: {player}')
+
+    computer = randint(0, 2)
+    print(f'Computer chose: {computer}')
+
+    if player == computer:
+        print('It\'s a tie!')
+    elif player == 0 and computer == 2:
+        print('You win!')
+    elif computer == 0 and player == 2:
+        print('You lose!')
+    elif player > computer:
+        print('You win!')
+    else:
+        print('You lose!')
 
 
 if __name__ == '__main__':
